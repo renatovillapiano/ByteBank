1d7fec1d6f984c3ab79a38f8eb655faf8e15d75d
renat <Tommy@001>
2024-11-09 19:56:19 -0300

    criação DAO - camada de acesso a dados

--- byte-bank-alura-curso_jdbc_inicio/src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
+++ byte-bank-alura-curso_jdbc_inicio/src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
@@ -1,13 +1,24 @@
 package br.com.alura.bytebank.domain.conta;
 
+import br.com.alura.bytebank.ConnectionFactory;
 import br.com.alura.bytebank.domain.RegraDeNegocioException;
 import br.com.alura.bytebank.domain.cliente.Cliente;
 
 import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.Set;
 
 public class ContaService {
+
+    private ConnectionFactory connection;
+
+    public ContaService(){
+        this.connection = new ConnectionFactory();
+    }
 
     private Set<Conta> contas = new HashSet<>();
 
@@ -21,13 +32,11 @@ public class ContaService {
     }
 
     public void abrir(DadosAberturaConta dadosDaConta) {
-        var cliente = new Cliente(dadosDaConta.dadosCliente());
-        var conta = new Conta(dadosDaConta.numero(), cliente);
-        if (contas.contains(conta)) {
-            throw new RegraDeNegocioException("Já existe outra conta aberta com o mesmo número!");
-        }
 
-        contas.add(conta);
+        Connection conn = connection.recuperarConexao();
+
+        new ContaDAO(conn).salvar(dadosDaConta);
+
     }
 
     public void realizarSaque(Integer numeroDaConta, BigDecimal valor) {
